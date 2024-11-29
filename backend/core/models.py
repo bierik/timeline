de9f8ffc3a4ec2fb4ae65537527e9ee19864921c
Kevin Bieri <bierike@gmail.com>
2024-11-29 22:12:16 +0100

    Fix ruff complaints

--- backend/core/models.py
+++ backend/core/models.py
@@ -4,4 +4,4 @@ from core.image.models import Image
 from core.people.models import Person
 from core.role.models import Role
 
-__all__ = ["User", "Event", "Image", "Person", "Role"]
+__all__ = ["Event", "Image", "Person", "Role", "User"]
